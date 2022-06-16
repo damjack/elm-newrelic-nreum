afdd92d7ff74cb6763525508ba0ca90520989cce
damjack <giacomello.damiano@gmail.com>
2022-06-16 23:19:11 +0200

    Ignore local directory. Add elm review and try to define tests

--- packages/elm-newrelic-nreum-elm/example/webpack.config.js
+++ packages/elm-newrelic-nreum-elm/example/webpack.config.js
@@ -4,16 +4,13 @@ const path = require('path');
 const fs = require('fs');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const extractMiniCss = new MiniCssExtractPlugin({
-  filename: 'src/[name].css',
-});
 
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 
 module.exports = {
   mode: 'development',
+  context: resolveApp('./src'),
   entry: [
     resolveApp('./src/index.js'),
   ],
@@ -21,11 +18,11 @@ module.exports = {
     pathinfo: true,
     filename: 'static/js/bundle.js',
     chunkFilename: 'static/js/[name].chunk.js',
-    publicPath: '',
+    publicPath: '/',
   },
   resolve: {
     modules: ['node_modules'],
-    extensions: ['.js', '.elm', '.scss', '.mjs'],
+    extensions: ['.js', '.elm', '.mjs'],
   },
   module: {
     noParse: /\.elm$/,
@@ -39,54 +36,22 @@ module.exports = {
         use: 'import-glob-loader',
       },
       {
-        test: /\.s[ac]ss$/,
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          { loader: require.resolve('css-loader') },
-          {
-            loader: require.resolve('postcss-loader'),
-            options: {
-              postcssOptions: {
-                plugins: ['autoprefixer'],
-              }
-            },
-          },
-          {
-            loader: require.resolve('sass-loader'),
-            options: {
-              implementation: require('node-sass'),
-              sourceMap: false,
-              sassOptions: {
-                indentedSyntax: false,
-              },
-            },
-          },
-        ],
-      },
-      {
         test: /\.m?js$/,
         exclude: [/[/\\\\]elm-stuff[/\\\\]/, /[/\\\\]node_modules[/\\\\]/],
-        include: resolveApp('./src/'),
+        include: '/',
         loader: require.resolve('babel-loader'),
         options: {
           presets: [
             [
               require.resolve('@babel/preset-env'),
               {
-                // `entry` transforms `@babel/polyfill` into individual requires for
-                // the targeted browsers. This is safer than `usage` which performs
-                // static code analysis to determine what's required.
-                // This is probably a fine default to help trim down bundles when
-                // end-users inevitably import '@babel/polyfill'.
                 useBuiltIns: 'entry',
                 corejs: 3,
-                // Do not transform modules to CJS
                 modules: false,
               },
             ],
           ],
           plugins: [
-            // Polyfills the runtime needed for async/await and generators
             [
               require('@babel/plugin-transform-runtime').default,
               {
@@ -97,8 +62,6 @@ module.exports = {
           ],
         },
       },
-      // Process any JS outside of the app with Babel.
-      // Unlike the application JS, we only compile the standard ES features.
       {
         test: /\.m?js$/,
         use: [
@@ -110,10 +73,8 @@ module.exports = {
               configFile: false,
               presets: [
                 [
-                  // Latest stable ECMAScript features
                   require('@babel/preset-env').default,
                   {
-                    // Do not transform modules to CJS
                     modules: false,
                   },
                 ],
@@ -126,15 +87,16 @@ module.exports = {
       },
       {
         test: /\.elm$/,
-        include: resolveApp('./src/'),
-        exclude: [/[/\\\\]elm-stuff[/\\\\]/, /[/\\\\]node_modules[/\\\\]/],
+        include: resolveApp('./src'),
+        exclude: [/elm-stuff/, /node_modules/],
         use: [
           {
             loader: require.resolve('elm-hot-webpack-loader'),
+            options: {},
           },
           {
             loader: require.resolve('string-replace-loader'),
-            query: {
+            options: {
               search: '%PUBLIC_URL%',
               replace: '',
               flags: 'g',
@@ -146,17 +108,15 @@ module.exports = {
           {
             loader: require.resolve('elm-webpack-loader'),
             options: {
-              verbose: true,
-              // If ELM_DEBUGGER was set to "false", disable it. Otherwise
-              // for invalid values, "true" and as a default, enable it
               debug: true,
+              optimize: false,
+              cwd: resolveApp('.'),
               pathToElm: require.resolve('elm/bin/elm'),
-              forceWatch: true,
+              runtimeOptions: ['-A128M', '-H128M', '-n8m'],
             },
           },
         ],
       },
-
       {
         exclude: [
           /\.html$/,
@@ -164,8 +124,6 @@ module.exports = {
           /\.mjs$/,
           /\.elm$/,
           /\.css$/,
-          /\.scss$/,
-          /\.sass$/,
           /\.json$/,
           /\.svg$/,
         ],
@@ -175,17 +133,10 @@ module.exports = {
           name: 'static/media/[name].[hash:8].[ext]',
         },
       },
-
-      {
-        test: /\.(jpg|svg|png|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: require.resolve('file-loader'),
-        options: { name: 'static/media/[name].[hash:8].[ext]' }
-      },
     ],
   },
 
   plugins: [
-    extractMiniCss,
     new HtmlWebpackPlugin({
       inject: true,
       template: resolveApp('./index.html'),
@@ -194,11 +145,29 @@ module.exports = {
   ],
 
   performance: false,
-  watch: true,
-
   devServer: {
-    inline: true,
+    client: {
+      logging: 'info',
+      overlay: true,
+      progress: false,
+      webSocketURL: 'ws://localhost:8000/ws',
+    },
+    webSocketServer: 'ws',
+    compress: true,
+    historyApiFallback: true,
     hot: true,
-    stats: 'errors-only'
-  }
+    liveReload: true,
+    static: {
+      directory: resolveApp('.'),
+      publicPath: '/',
+      watch: {
+        poll: true,
+        ignored: /node_modules/,
+      },
+    },
+    https: false,
+    host: '0.0.0.0',
+    open: true,
+    port: 8000,
+  },
 };
